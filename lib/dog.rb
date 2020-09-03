eb0a875535817d5d86ecf113aa93a3165c7a9dde
Taylor Sieling <taylorasieling@gmail.com>
2020-09-03 01:18:30 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  
+  attr_accessor = :name 
+  attr_accessor = :breed 
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def initialize(breed)
+    @breed = breed
+  end
+  
+end 
